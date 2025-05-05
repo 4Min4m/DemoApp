07e3608981e25e154ad9b3fd09ebb6f58e22be83
Amin <36745993+4Min4m@users.noreply.github.com>
2025-05-05 17:49:03 +0000

    update bucket name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-demo"
+    bucket         = "my-terraform-state-demo-231"
     key            = "demo/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
