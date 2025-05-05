8e819722b9478314a8e229cad70b26bd3728356d
Amin <36745993+4Min4m@users.noreply.github.com>
2025-05-05 18:06:02 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -42,18 +42,3 @@ resource "aws_s3_object" "index_html" {
   source       = "app/index.html"
   content_type = "text/html"
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-locks"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  tags = {
-    Name        = "terraform-locks"
-    Environment = var.environment
-    Project     = "Demo"
-  }
-}
