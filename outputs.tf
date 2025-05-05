31d984f9eca2ca12653b508343c5b610a8f45418
Amin <36745993+4Min4m@users.noreply.github.com>
2025-05-05 21:22:36 +0000

    update output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "web_url" {
   description = "URL of the web application (Dev environment)"
-  value       = "http://${aws_instance.web.public_ip}"
+  value       = "Web app deployed via Auto Scaling Group. Check EC2 instances with tag Environment=dev for Public IP."
   depends_on  = [module.ec2]
 }
