f25ca9f838a43de010535b93a79dc6a07b17396f
Amin <36745993+4Min4m@users.noreply.github.com>
2025-05-08 11:17:29 +0000

    simplifing files

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "random" {}
-
 module "vpc" {
   source      = "./modules/vpc"
   environment = var.environment
@@ -19,14 +17,8 @@ module "ec2" {
   instance_type = "t2.micro"
 }
 
-resource "random_string" "bucket_suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
 resource "aws_s3_bucket" "backup" {
-  bucket = "my-app-backup-demo-${random_string.bucket_suffix.result}"
+  bucket = "my-app-backup-demo"
   tags = {
     Name        = "${var.environment}-backup"
     Environment = var.environment
